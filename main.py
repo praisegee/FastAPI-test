64ecd41e43acdf6bcceb25b797495ca85abf9466
PraiseGod <99336188+praisegee@users.noreply.github.com>
2022-10-07 07:45:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,18 @@
+import os
+import json
+import uvicorn
 from fastapi import FastAPI
-from pydantic import BaseModel
 
 app = FastAPI()
 
-class Msg(BaseModel):
-    msg: str
+with open("data/test.json") as file:
+	data = json.load(file)
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World. Welcome to FastAPI!"}
+def fetch_all():
+	return data
 
 
-@app.get("/path")
-async def demo_get():
-    return {"message": "This is /path endpoint, use a post request to transform the text to uppercase"}
-
-
-@app.post("/path")
-async def demo_post(inp: Msg):
-    return {"message": inp.msg.upper()}
-
-
-@app.get("/path/{path_id}")
-async def demo_get_path_id(path_id: int):
-    return {"message": f"This is /path/{path_id} endpoint, use post request to retrieve result"}
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT", default=5000), log_level="info")
